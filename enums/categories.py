4ab30b673d983ef3f951a2fda4756025b8001c68
todynyuk <todynyuk@solvd.com>
2023-03-23 18:30:56 +0200

    edit advanced test,delete files for rozetka test

--- enums/categories.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Categories(Enum):
-    LAPTOPS = "Ноутбуки",
-    SMARTPHONES = "Смартфони",
-
--- enums/subcategories.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class Sub_Categories(Enum):
-    MOBILE = "Мобільні",
-    MONOBLOC = "моноблоки"
