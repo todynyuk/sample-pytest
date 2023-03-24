27d289dbf75d1e23d6148dbb01ec780f33590779
todynyuk <todynyuk@solvd.com>
2023-03-24 16:24:15 +0200

    rename rozetka tests and test file add more one web-test for rozetka

--- tests/rozetka_main_page_search_test.py
+++ tests/rozetka_main_page_search_test.py
@@ -28,10 +28,11 @@ search_value = "Samsung"
 def test_test(driver):
     logger.info("Attaching label to test")
     attach_test_label("Test", "Assert")
-    assert  True
+    assert True
     logger.info("'test_test' test was successfully finished")
 
-def test_rozetka(driver):
+
+def test_rozetka_correct_search(driver):
     logger.info("Attaching labels, artifacts and artifacts references to test")
     attach_test_label("TestLabel", "Rozetka")
     attach_test_run_artifact_reference("Rozetka", "https://rozetka.com.ua/ua/")
@@ -51,7 +52,26 @@ def test_rozetka(driver):
     logger.info(goods_title_text)
     assert str(goods_title_text.lower()).__contains__(
         search_value.lower()), "Device description not contains search_value"
-    logger.info("'test_rozetka' test was successfully finished")
+    logger.info("'test_rozetka_correct_search' test was successfully finished")
+
+
+def test_rozetka_incorrect_search(driver):
+    logger.info("Attaching labels, artifacts and artifacts references to test")
+    attach_test_label("TestLabel", "Rozetka")
+    attach_test_run_artifact_reference("Rozetka", "https://rozetka.com.ua/ua/")
+    logger.info("Navigating to url: " + url)
+    driver.get(url=url)
+    attach_screenshot(driver)
+    logger.info("Performing search with value: " + "hgvhvg")
+    time.sleep(3)
+    driver.find_element(By.XPATH, "//input[contains(@class, 'search-form__input')]").send_keys("hgvhvg")
+    time.sleep(5)
+    driver.find_element(By.XPATH, "//button[contains(@class, 'search-form__submit')]").click()
+    attach_screenshot(driver)
+    time.sleep(10)
+    logger.info("Verify not_found_text is present")
+    assert driver.find_element(By.XPATH, "//span[@class='ng-star-inserted']").is_displayed()
+    logger.info("'test_rozetka_incorrect_search' test was successfully finished")
 
 
 def attach_screenshot(driver):
