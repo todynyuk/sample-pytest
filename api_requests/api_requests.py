3cafbbefce3362909a82aac815269e75add35cf9
todynyuk <todynyuk@solvd.com>
2023-03-29 12:50:32 +0300

    edit api test and methods for it

--- api_requests/api_requests.py
+++ api_requests/api_requests.py
@@ -7,14 +7,11 @@ BASE_URL = "https://reqres.in/"
 FILE_PATH = "/Users/tarasodynyuk/PycharmProjects/web_tests/resources/api/"
 
 
-class ReqresInApi:
-
-    @staticmethod
-    def get_list_users():
-        endpoint = "api/users?page=2"
-        get_url = BASE_URL + endpoint
-        json_rs_path = f"{FILE_PATH}get/rs.json"
-        response = requests.get(get_url)
-        response_json = json.loads(response.text)
-        write_response_to_file(json_rs_path, response_json)
-        return response
+def get_list_users():
+    endpoint = "api/users?page=2"
+    get_url = BASE_URL + endpoint
+    json_rs_path = f"{FILE_PATH}get/rs.json"
+    response = requests.get(get_url)
+    response_json = json.loads(response.text)
+    write_response_to_file(json_rs_path, response_json)
+    return response
