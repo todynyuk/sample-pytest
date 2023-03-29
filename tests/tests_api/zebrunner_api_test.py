f397ed402c69eae054f4891b08b0ba23b0c5cc97
todynyuk <todynyuk@solvd.com>
2023-03-29 16:38:39 +0300

    modify api zebrunner test-add attach screenshot

--- tests/tests_api/zebrunner_api_test.py
+++ tests/tests_api/zebrunner_api_test.py
@@ -4,6 +4,7 @@ import requests
 from pytest_zebrunner import attach_test_run_label, attach_test_run_artifact_reference, attach_test_label
 from pytest_zebrunner.zebrunner_logging import ZebrunnerHandler
 from pytest_zebrunner import CurrentTestRun
+from utils.attachments import attach_screenshot
 
 logger = logging.getLogger(__name__)
 logger.addHandler(ZebrunnerHandler())
@@ -26,6 +27,8 @@ def test_request_response(driver):
     attach_test_run_artifact_reference("Zebrunner", "https://solvdinternal.zebrunner.com")
     logger.info("Navigating to url: " + url)
     driver.get(url=url)
+    attach_screenshot(driver)
+    logger.info("test_request_response on " + "https://solvdinternal.zebrunner.com")
     response = requests.get(url)
     assert response.ok, "Response not as expected."
     logger.info("'test_request_response' was successfully finished")
