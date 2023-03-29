0989132bd75caee06fb3b1edb67c08648c2a1bec
todynyuk <todynyuk@solvd.com>
2023-03-29 14:27:49 +0300

    delete request api test and add zebrunner api test

--- tests/reqres_api_test.py
+++ tests/reqres_api_test.py
@@ -1,6 +1,7 @@
 import logging
 import time
 
+import requests
 from pytest_zebrunner import attach_test_run_label, attach_test_run_artifact_reference, attach_test_label
 from pytest_zebrunner.zebrunner_logging import ZebrunnerHandler
 from pytest_zebrunner import CurrentTestRun
@@ -21,17 +22,7 @@ attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
 CurrentTestRun.set_locale("en_US")
 CurrentTestRun.set_build("TR build version")
 
-url = "https://reqres.in/api/users?page=2"
-def test_get_list_users(driver):
-    logger.info("Attaching labels, artifacts and artifacts references to test")
-    attach_test_label("TestLabel", "ReqresAPI")
-    attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
-    logger.info("Navigating to url: " + url)
-    response = ReqresInApi.get_list_users(driver)
-    attach_screenshot(driver)
-    request_method = response.request.method
-    assert request_method == 'GET'
-    assert Validations.valid_status_code(response, 200), "Status code not as expected."
-    assert Validations.response_time(response, 600), \
-        f"Response time({round(response.elapsed.total_seconds() * 1000)}) is more than {600} ms."
-    logger.info("Successful GET request(list_users)")
+
+def test_request_response(driver):
+    response = requests.get('https://solvdinternal.zebrunner.com')
+    assert response.ok, "Response not as expected."
