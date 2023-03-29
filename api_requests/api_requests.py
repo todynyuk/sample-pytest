ed8ac49a2494180f65456b631275b2bca96a591e
todynyuk <todynyuk@solvd.com>
2023-03-29 12:37:57 +0300

    add test_get_list_users for reqestapi and methods for it

---
+++ api_requests/api_requests.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+from utils.json_utils import write_response_to_file
+
+BASE_URL = "https://reqres.in/"
+FILE_PATH = "/Users/tarasodynyuk/PycharmProjects/web_tests/resources/api/"
+
+
+class ReqresInApi:
+
+    @staticmethod
+    def get_list_users():
+        endpoint = "api/users?page=2"
+        get_url = BASE_URL + endpoint
+        json_rs_path = f"{FILE_PATH}get/rs.json"
+        response = requests.get(get_url)
+        response_json = json.loads(response.text)
+        write_response_to_file(json_rs_path, response_json)
+        return response
---
+++ tests/reqres_api_test.py
@@ -0,0 +1,34 @@
+import logging
+import time
+
+from pytest_zebrunner import attach_test_run_label, attach_test_run_artifact_reference, attach_test_label
+from pytest_zebrunner.zebrunner_logging import ZebrunnerHandler
+from pytest_zebrunner import CurrentTestRun
+
+from api_requests.api_requests import ReqresInApi
+from api_validations.api_validations import Validations
+
+logger = logging.getLogger(__name__)
+logger.addHandler(ZebrunnerHandler())
+logger.setLevel(logging.INFO)
+
+attach_test_run_label("TestRunLabelRunName", "PyTest")
+attach_test_run_label("TestRunLabelResourceName", "ReqresAPI")
+
+attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
+
+CurrentTestRun.set_locale("en_US")
+CurrentTestRun.set_build("TR build version")
+
+def test_get_list_users():
+    logger.info("Attaching labels, artifacts and artifacts references to test")
+    attach_test_label("TestLabel", "ReqresAPI")
+    attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
+    logger.info("Navigating to url: " + "https://reqres.in/api/users?page=2")
+    response = ReqresInApi.get_list_users()
+    request_method = response.request.method
+    assert request_method == 'GET'
+    assert Validations.valid_status_code(response, 200), "Status code not as expected."
+    assert Validations.response_time(response, 600), \
+        f"Response time({round(response.elapsed.total_seconds() * 1000)}) is more than {600} ms."
+    logger.info("Successful GET request(list_users)")
