3fd4d36ef78380c4060c4e776a84df8e64a46f82
todynyuk <todynyuk@solvd.com>
2023-03-29 14:24:31 +0300

    modify api test-add screenshot

--- tests/reqres_api_test.py
+++ tests/reqres_api_test.py
@@ -1,13 +1,13 @@
 import logging
 import time
 
-import requests
 from pytest_zebrunner import attach_test_run_label, attach_test_run_artifact_reference, attach_test_label
 from pytest_zebrunner.zebrunner_logging import ZebrunnerHandler
 from pytest_zebrunner import CurrentTestRun
 
 from api_requests.api_requests import ReqresInApi
 from api_validations.api_validations import Validations
+from utils.attachments import attach_screenshot
 
 logger = logging.getLogger(__name__)
 logger.addHandler(ZebrunnerHandler())
@@ -21,13 +21,14 @@ attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
 CurrentTestRun.set_locale("en_US")
 CurrentTestRun.set_build("TR build version")
 
-
+url = "https://reqres.in/api/users?page=2"
 def test_get_list_users(driver):
     logger.info("Attaching labels, artifacts and artifacts references to test")
     attach_test_label("TestLabel", "ReqresAPI")
     attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
-    logger.info("Navigating to url: " + "https://reqres.in/api/users?page=2")
-    response = requests.get(driver.get(url="https://reqres.in/api/users?page=2"))
+    logger.info("Navigating to url: " + url)
+    response = ReqresInApi.get_list_users(driver)
+    attach_screenshot(driver)
     request_method = response.request.method
     assert request_method == 'GET'
     assert Validations.valid_status_code(response, 200), "Status code not as expected."
