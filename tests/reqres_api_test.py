2239ad957e9aab5ffe80cf90962b14f3e3a15931
todynyuk <todynyuk@solvd.com>
2023-03-29 13:01:56 +0300

    edit api test and methods

--- tests/reqres_api_test.py
+++ tests/reqres_api_test.py
@@ -5,8 +5,8 @@ from pytest_zebrunner import attach_test_run_label, attach_test_run_artifact_ref
 from pytest_zebrunner.zebrunner_logging import ZebrunnerHandler
 from pytest_zebrunner import CurrentTestRun
 
-from api_requests.api_requests import get_list_users
-from api_validations.api_validations import valid_status_code, response_time
+from api_requests.api_requests import ReqresInApi
+from api_validations.api_validations import Validations
 
 logger = logging.getLogger(__name__)
 logger.addHandler(ZebrunnerHandler())
@@ -20,16 +20,16 @@ attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
 CurrentTestRun.set_locale("en_US")
 CurrentTestRun.set_build("TR build version")
 
+
 def test_get_list_users(driver):
     logger.info("Attaching labels, artifacts and artifacts references to test")
     attach_test_label("TestLabel", "ReqresAPI")
     attach_test_run_artifact_reference("ReqresAPI", "https://reqres.in/")
     logger.info("Navigating to url: " + "https://reqres.in/api/users?page=2")
-    driver.get(url="https://reqres.in/api/users?page=2")
-    response = get_list_users()
+    response = ReqresInApi.get_list_users(driver)
     request_method = response.request.method
     assert request_method == 'GET'
-    assert valid_status_code(response, 200), "Status code not as expected."
-    assert response_time(response, 600), \
+    assert Validations.valid_status_code(response, 200), "Status code not as expected."
+    assert Validations.response_time(response, 600), \
         f"Response time({round(response.elapsed.total_seconds() * 1000)}) is more than {600} ms."
     logger.info("Successful GET request(list_users)")
