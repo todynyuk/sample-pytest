3fa5359b06fdbf4785371ce078d7dd2b6a68cbcb
todynyuk <todynyuk@solvd.com>
2023-04-07 11:20:13 +0300

    fix problem file_not_found for open weather tests

--- api_methods/open_weather_methods.py
+++ api_methods/open_weather_methods.py
@@ -4,7 +4,7 @@ import json
 from utils.json_utils import write_response_to_file
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-json_rs_path = "/Users/tarasodynyuk/Documents/pytest_demo/python-agent-samples/sample-pytest/resources/api/get/rs.json"
+json_rs_path = "resources/api/get/rs.json"
 
 def get_open_weather_info_by_city_name(city, api_key):
     url = BASE_URL + f"q={city}" + f"&appid={api_key}&units=metric"
