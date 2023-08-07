b8f72e926e81c726d894802a3e52c92674e897f8
todynyuk <todynyuk@solvd.com>
2023-08-07 18:08:28 +0300

    rewrite browser option and selenium grid

--- conftest.py
+++ conftest.py
@@ -6,15 +6,17 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def driver():
-    zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "http://127.0.0.1:4444")
+    # zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "http://127.0.0.1:4444")
+    zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "https://solvdinternal:O9JjPcqxJ0hPs685@engine.zebrunner.com/wd/hub")
 
 
-    options = webdriver.ChromeOptions()
-    options.platform_name = "linux"
-    options.browser_version = "latest"
-    # options = webdriver.FirefoxOptions()
+    # options = webdriver.ChromeOptions()
     # options.platform_name = "linux"
+    # options.browser_version = "latest"
+    options = webdriver.FirefoxOptions()
+    options.platform_name = "linux"
     # options.browser_version = "108.0"
+    options.browser_version = "latest"
     options.set_capability("enableVideo", "true")
 
     driver = webdriver.Remote(
